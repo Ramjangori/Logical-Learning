2e1cdd1484dba79166a0259a714f3fa08a6f32a8
MOH RAMJAN <ramjangori6666@gmail.com>
2025-12-10 09:25:06 +0530

    LCM OF NUMBER

---
+++ LCM.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int n1,n2;
+    printf("Enter Two Numbers For LCM :");
+    scanf("%d%d",&n1,&n2);
+    int i=2;
+    while(n1>1 || n2>1){
+        if(n1%i==0 && n2%i==0){
+            printf("%d ",i);
+            n1 = n1/i;
+            n2 = n2/i;
+        }
+        else if(n1%i==0){
+            printf("%d ",i);
+            n1 = n1/i;
+        }
+        else if(n2%i==0){
+            printf("%d ",i);
+            n2 = n2/i;
+        }
+        else i++;
+        
+    }
+    return 0;
+}
