ea697362d5dc4449e6d994907d7a479e89e9d7e6
MOH RAMJAN <ramjangori6666@gmail.com>
2025-12-16 08:58:42 +0530

    added binary to octal program next start from line 56

--- Number-Conversion.c
+++ Number-Conversion.c
@@ -27,6 +27,32 @@ int main(){
 
         }     
         if(m==2){
+             int N,Octal=0;
+         int OctalPlace=1;
+        printf("Enter Binary Number : ");
+    scanf("%d",&N);
+    while(N>0){
+        
+        int pos=1;
+        int pair ;
+        
+        int OctalPair=0;
+        pair = N%1000;
+        N = N/1000;
+        while(pair>0){
+            
+            OctalPair = OctalPair + (pair%10)*pos;
+            pos = pos*2;
+            pair = pair/10;
+            
+        }
+        Octal = Octal + OctalPair*OctalPlace;
+        OctalPlace= OctalPlace*10;
+        
+    }
+    printf("Octal is : %d",Octal);
+        }
+        if(m==3){
             
         }
      }
