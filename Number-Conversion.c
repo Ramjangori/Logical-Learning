339bab92ba38962fdb68a802f72fed4fb5c6cc5d
MOH RAMJAN <ramjangori6666@gmail.com>
2025-12-16 13:12:16 +0530

    all files created only hexadecimal reamining

--- Number-Conversion.c
+++ Number-Conversion.c
@@ -4,13 +4,13 @@ int main(){
     int conversion;
 	 printf("--Number Conversion System--\n");
 	 printf("Enter What Type Number you Will Enter..\n");
-	 printf("Press 1->Binary\n Press 2->Decimal\n Press 3->Octal\n Press 4->Hexadecimal\n");
+	 printf("Press 1->Binary\nPress 2->Decimal\nPress 3->Octal\nPress 4->Hexadecimal\n");
      scanf("%d",&n);
 
      if(n==1){
         int m;
         printf("What Type Number You Want to Get..\n");
-        printf("Press 1->Decimal\n Press 2->Octal\n press 3->Hexadecimal\n");
+        printf("Press 1->Decimal\nPress 2->Octal\npress 3->Hexadecimal\n");
         scanf("%d",&m);
         if(m==1){
 
@@ -58,7 +58,8 @@ int main(){
      }
 	 else if(n==2){
         int m;
-        printf("Press 1->Binary\n Press 2->Octal\n Press 3-> Hexadecimal\n");
+        printf("What type Number  You Want to Get..\n");
+        printf("Press 1->Binary\nPress 2->Octal\nPress 3->Hexadecimal\n");
         scanf("%d",&m);
         if(m==1){
             int num,binary=0;
@@ -75,8 +76,60 @@ int main(){
     printf("Binary is %d :" , binary);
         }
         if(m==2){
-            
+            int num;
+           printf("Enter Number :");
+           scanf("%d",&num);
+           int octal=0;
+          int up = 1;
+    while(num>0){
+        int rem = num%8;
+        octal = octal + rem*up;
+        up = up*10;
+        num = num/8;
+
+    }
+    printf("Octal Number is %d :",octal);
         } 
     
-    }	
+    }
+     else if(n==3)	{
+        int m;
+        printf("Enter What type Number want to get ..\n");
+        printf("Press 1->Binary\nPress 2->Decimal\nPress3->Hexadecimal\n");
+        scanf("%d",&m);
+        if(m==1){
+            int num , decimal = 0 , up=1;
+          printf("Enter Octal num :");
+         scanf("%d",&num);
+       while(num>0){
+        int rem = num%10;
+        decimal = decimal + rem*up;
+        up = up*8;
+        num = num/10;
+    }
+    int binary=0;
+    int position=1;
+    while(decimal>=1){
+        binary = binary + (decimal%2)*position;
+        position = position*10;
+        decimal = decimal/2;
+        
+    }
+ 
+    printf("Binary is %d :" , binary);
+     }
+        if(m==2){
+             int num , decimal = 0 , up=1;
+          printf("Enter Octal num :");
+         scanf("%d",&num);
+       while(num>0){
+        int rem = num%10;
+        decimal = decimal + rem*up;
+        up = up*8;
+        num = num/10;
+    } 
+     printf("Decimal is %d :",decimal);
+        }
+}
+  return 0;
 }
